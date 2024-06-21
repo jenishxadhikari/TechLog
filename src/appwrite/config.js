b790a640e9565ca3f5bc1ca5d4f0fb1f9acfc5c4
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-21 20:32:36 +0545

    feat: Implement operations for managing posts using Appwrite

---
+++ src/appwrite/config.js
@@ -0,0 +1,90 @@
+import { Client, Databases, Query } from "appwrite";
+import conf from "../conf/conf";
+
+class Service {
+  client = new Client();
+  databases;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl);
+    this.client.setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      return await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async listPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const service = new Service();
+
+export default service;
