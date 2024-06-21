93ca23a4f07def38684ec2db4d1d62c02f587556
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2024-06-21 19:43:15 +0545

    Implement AuthService class with account management methods

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,7 +1,9 @@
-export const conf = {
-    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    projectId : String(import.meta.env.VITE_PROJECT_ID),
-    databaseId : String(import.meta.env.VITE_DATABASE_ID),
-    collectionId : String(import.meta.env.VITE_COLLECTION_ID),
-    bucketId : String(import.meta.env.VITE_BUCKET_ID),
-}
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+};
+
+export default conf;
